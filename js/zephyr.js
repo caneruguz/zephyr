da12560970960e204ce597714766a64ed3fdbb71
Caner Mac <caneruguz@gmail.com>
2014-01-25 00:46:52 -0500

    Further development with knockout

--- js/zephyr.js
+++ js/zephyr.js
@@ -4,18 +4,45 @@
  */
 
 function Comments(id, username, content, replySource) {
+/*
 	this.id = id;
 	this.username = username;
 	this.content = content;
 	this.replySource = replySource;
+*/
+	
+	var html = "<div class='cm-commentBox' postid='"+id+"'> <div class='cm-commentText' ><b>"+username+"</b><span class='cm-boxMenu cm-reply'> Reply</span><br /><span> "+content+"</span></div></div>"; 
+	$('.cm-commentBox[postid="'+replySource+'"]').append(html); 
 }
 
-function CommentBoxModel() {
-	var self = this;
+function CommentBoxModel(loadData) {
+	var self = this; 	
+	
+	// Options
+	self.sidebarWidth = 400; // The window width at which the sidebar disappears and integrated comments kick in. This is useful for responsiveness. 
+	
 	self.commentMode = false;
 	self.dragState = false;
-	self.load = ko.observableArray([
-	new Comments(1, "Jeff", "This is a comment", 0), new Comments(2, "Josh", "Another comment to text", 0), new Comments(3, "Caner", "Reply to a comment", 1)]);
+	
+	// placeholders 
+	self.latestComment = '',
+	self.currentUser = 'Caner'; 
+	
+	// window sizes 
+	self.sidebar = ko.observable(true); 
+	self.width = $(window).width(); 
+	$(window).resize(function () { 
+		$(window).width() < self.sidebarWidth ? self.sidebar(false) : self.sidebar(true); 
+			
+		
+	}); 
+	self.comments = ko.observableArray(); 	
+	self.totalComments = loadData[loadData.length-1].id; 
+	// load existing comments 
+	for(var i = 0; i < loadData.length; i++){
+		var m = loadData[i]; 
+		self.comments.push(new Comments(m.id, m.username, m.content, m.replySource));
+	}
 	self.tabDragDown = function() {
 		self.dragState = true;
 		console.log(self.dragState);
@@ -53,5 +80,10 @@ function CommentBoxModel() {
 			self.commentMode = true;
 		}
 	}
+	self.makeComment = function() {
+        self.comments.push(new Comments(self.totalComments+1, self.currentUser, self.latestComment, 0));
+    }
+	
+	//window resize function 
+   
 }
-ko.applyBindings(new CommentBoxModel());
